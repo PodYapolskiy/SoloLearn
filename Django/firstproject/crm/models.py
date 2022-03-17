82edc86d1239ea2f3f83807fbe7837880f5ce875
Anatoly <podyapolskiyaa@gmail.com>
2022-03-17 22:04:02 +0300

    Upgrade crm app, add comments and statuses

--- Django/firstproject/crm/models.py
+++ Django/firstproject/crm/models.py
@@ -1,13 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class StatusCrm(models.Model):
+    status_name = models.CharField(max_length=200, verbose_name="Название статуса")
+    
+    def __str__(self):
+        return self.status_name
+
+    class Meta:
+        """Обозначает то, как будет отображаться класс в единственном и множественном числе"""
+        verbose_name = "Статус"
+        verbose_name_plural = "Статусы"
 
 
 class Order(models.Model):
-    # id - создаётся автоматиески
+    """
+        Варианты параметра on_delete у ForeignKey
+    CASCAD - При удалении родителя, удаляются все потомки
+    SET_NULL - При удалении, потомки переходят в состояние NULL
+    PROTECT - Без возможности удаления
+    """
     order_dt = models.DateTimeField(auto_now=True)
     order_name = models.CharField(max_length=200, verbose_name="Имя")
-    order_phone = models.CharField(max_length=200, verbose_name="Телефон", )
+    order_phone = models.CharField(max_length=200, verbose_name="Телефон")
+    order_status = models.ForeignKey(StatusCrm, on_delete=models.PROTECT, null=True, blank=True, verbose_name="Статус")
 
     def __str__(self):
         return self.order_name
@@ -16,3 +32,18 @@ class Order(models.Model):
         """Обозначает то, как будет отображаться класс в единственном и множественном числе"""
         verbose_name = "Заказ"
         verbose_name_plural = "Заказы"
+
+
+class CommentCrm(models.Model):
+    # При удалении родителя (заказа), удалится и комментарий к нему
+    comment_binding = models.ForeignKey(Order, on_delete=models.CASCADE, verbose_name='Заявка')
+    comment_text = models.TextField(verbose_name="Текст комментария")
+    comment_dt = models.DateTimeField(auto_now=True, verbose_name="Дата создания")
+
+    def __str__(self):
+        return self.comment_text
+
+    class Meta:
+        """Обозначает то, как будет отображаться класс в единственном и множественном числе"""
+        verbose_name = "Комментарий"
+        verbose_name_plural = "Комментарии"
--- Django/firstproject/price/models.py
+++ Django/firstproject/price/models.py
@@ -13,11 +13,12 @@ class PriceCard(models.Model):
         verbose_name = "Цены"
         verbose_name_plural = "Цены"
 
+
 class PriceTable(models.Model):
     pt_title = models.CharField(max_length=200, verbose_name='Услуга')
     pt_old_price = models.CharField(max_length=200, verbose_name='Старая цена')
     pt_new_price = models.CharField(max_length=200, verbose_name='Новая цена')
-    
+
     def __str__(self):
         return self.pt_title
 
