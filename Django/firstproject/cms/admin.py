58747b0e843c9d7c1c01bd0eacdc178d3f3bccab
Anatoly <podyapolskiyaa@gmail.com>
2022-03-20 22:42:05 +0300

    Upgrade admin panel's views

--- Django/firstproject/cms/admin.py
+++ Django/firstproject/cms/admin.py
@@ -1,5 +1,26 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+
 from .models import CmsSlider
 
+
 # Register your models here.
-admin.site.register(CmsSlider)
+class CmsAdmin(admin.ModelAdmin):
+    # Последний элемент - результат работы метода get_image
+    list_display = ('cms_title', 'cms_text', 'cms_css', 'get_image')
+    list_display_links = ('cms_title',)
+    list_editable = ('cms_css',)
+
+    fields = ('cms_title', 'cms_text', 'cms_css', 'cms_img', 'get_image')
+    readonly_fields = ('get_image',)  # Так как поле специфичное
+
+    def get_image(self, obj):
+        # Форматируемая html строка
+        if obj.cms_img:
+            return mark_safe(f'<img src="{obj.cms_img.url}" width="80px">')
+        return 'Нет картинки'
+
+    get_image.short_description = 'Миниатюра'  # Название колонки
+
+
+admin.site.register(CmsSlider, CmsAdmin)
--- Django/firstproject/crm/admin.py
+++ Django/firstproject/crm/admin.py
@@ -1,6 +1,29 @@
 from django.contrib import admin
 from .models import Order, StatusCrm, CommentCrm
 
-admin.site.register(Order)
+
+class Comment(admin.StackedInline):
+    model = CommentCrm  # Обязательное поле, указывающее на класс использемой модели
+    fields = ('comment_dt', 'comment_text')  # Поля, которые будут показываться
+    readonly_fields = ('comment_dt',)
+    extra = 1  # Кол-во пустых полей, которыми можно воспользоваться, чтобы создать новые записи
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'order_name', 'order_phone', 'order_status', 'order_dt')  # Имена полей модели
+    list_display_links = ('id', 'order_name')  # Ссылки полей
+    search_fields = ('id', 'order_name', 'order_phone', 'order_dt')  # Поля, по которым можно искать
+    list_filter = ('order_status',)  # Фильтр записей по статусу
+    list_editable = ('order_status', 'order_phone')  # Какие поля можно изменять прямо в таблице
+    list_per_page = 4  # Сколько записей отражается в скрытом виде
+    list_max_show_all = 100  # Сколько записей отображается после нажатия show all
+
+    fields = ('id', 'order_status', 'order_dt', 'order_name', 'order_phone')  # Порядок полей в модели заказа
+    readonly_fields = ('id', 'order_dt')  # Поля, которые нельзя изменить
+    
+    inlines = (Comment, )
+
+
+admin.site.register(Order, OrderAdmin)
 admin.site.register(StatusCrm)
 admin.site.register(CommentCrm)
