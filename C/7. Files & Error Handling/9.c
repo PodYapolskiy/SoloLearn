cb49fd943c3a5cf8e0de415543088cc52c691043
Anatoly <podyapolskiyaa@gmail.com>
2022-04-02 00:28:42 +0300

    C: 7. Files & Error Handling / Error Codes

---
+++ C/7. Files & Error Handling/9.c
@@ -0,0 +1,13 @@
+// Error Codes
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+
+extern int errno;
+
+int main()
+{
+    for (int x = 0; x < 135; x++)
+        fprintf(stderr, "%d: %s\n", x, strerror(x));
+}
