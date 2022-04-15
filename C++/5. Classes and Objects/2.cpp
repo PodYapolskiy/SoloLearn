98e30a2c2cc7b954ff5b6006476e565020a805b6
Anatoly <podyapolskiyaa@gmail.com>
2022-04-16 00:10:50 +0300

    C++: 5. Classes and Objects / Abstraction

---
+++ C++/5. Classes and Objects/2.cpp
@@ -0,0 +1,21 @@
+// Abstraction
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    /*
+            Abstraction
+        Data abstraction is the concept of providing only essential information to the outside world.
+        It's a process of representing essential features without including implementation details.
+        Abstraction means, that we can have an idea or a concept that is completely separate from any specific instance.
+
+        Example:
+        cout << "Hello!" << endl;
+
+        There is no need to understand how cout will display the text on the user's screen.
+        The only thing you need to know to be able to use it is the public interface.
+    */
+
+    return 0;
+}
