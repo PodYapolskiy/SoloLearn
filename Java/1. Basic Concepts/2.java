bd9d93b790c158628e1a5158ea11c91888c2d4f8
Anatoly <podyapolskiyaa@gmail.com>
2022-04-30 00:06:30 +0300

    Java: 1. Basic Concepts / Variables

---
+++ Java/1. Basic Concepts/2.java
@@ -0,0 +1,18 @@
+class Variables {
+    /*
+     * type name = value;
+     */
+    public static void main(String[] args) {
+        int someInt = 123;
+        double someDouble = 3.1416;
+        String someString = "Hello, world!";
+        char someChar = 'A';
+        boolean someBool = true;
+
+        System.out.println(someInt);
+        System.out.println(someDouble);
+        System.out.println(someString);
+        System.out.println(someChar);
+        System.out.println(someBool);
+    }
+}
