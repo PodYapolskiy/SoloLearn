af6abe84439141c6236a6b4a7240fb4ead470b07
Anatoly <podyapolskiyaa@gmail.com>
2022-05-01 00:09:14 +0300

    Java: 1. Basic Concepts / Strings

---
+++ Java/1. Basic Concepts/5.java
@@ -0,0 +1,12 @@
+class Strings {
+    /*
+     * A String is an object that represents a sequence of characters.
+     */
+    public static void main(String[] args) {
+        String s = "SoloLearn";
+        System.out.println(s);
+
+        String firstName = "Anatoly", lastName = "Podyapolsky";
+        System.out.println("It's me " + firstName + " " + lastName); // String concatenation
+    }
+}
