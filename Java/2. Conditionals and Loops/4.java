df79bdff1ef84fd02650312d37c1c50b85ecd967
Anatoly <podyapolskiyaa@gmail.com>
2022-05-05 00:02:31 +0300

    Java: 2. Conditionals and Loops / While Loop

---
+++ Java/2. Conditionals and Loops/4.java
@@ -0,0 +1,16 @@
+class WhileLoop {
+    /*
+     * A while loop statement repeatedly executes a target statement as long as a
+     * given condition is true.
+     */
+    public static void main(String[] args) {
+        int x = 6;
+
+        while (x < 10) {
+            System.out.println(x);
+            x++;
+        }
+
+        System.out.println("Loop ended");
+    }
+}
