a644f418972d8fe6e10b5509d53788bab4c65468
Anatoly <podyapolskiyaa@gmail.com>
2022-05-11 00:00:26 +0300

    Java: 4. Classes and Objects / Methods

---
+++ Java/4. Classes and Objects/1. Methods/Methods.java
@@ -0,0 +1,14 @@
+public class Methods {
+    static void sayHello(String name) {
+        System.out.println("Hello, " + name + "!");
+    }
+
+    /*
+     * Methods define behavior.
+     * A method is a collection of statements that are grouped together to perform
+     * an operation.
+     */
+    public static void main(String[] args) {
+        sayHello("Amy");
+    }
+}
