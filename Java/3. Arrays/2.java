00167758e8f1578b1933e73341e56334b5806e70
Anatoly <podyapolskiyaa@gmail.com>
2022-05-10 12:15:11 +0300

    Java: 3. Arrays / Multidimensional Arrays

---
+++ Java/3. Arrays/2.java
@@ -0,0 +1,10 @@
+class MultidimensionalArrays {
+    /*
+     * Multidimensional arrays are array that contain other arrays.
+     */
+    public static void main(String[] args) {
+        int[][] sample = { { 1, 2, 3 }, { 4, 5, 6 } };
+        int x = sample[1][0];
+        System.out.println(x); // 4
+    }
+}
