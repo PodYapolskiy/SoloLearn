34f80e08a396e329a004dd2af3b9bcf71e652b95
Anatoly <podyapolskiyaa@gmail.com>
2022-06-17 00:05:18 +0300

    Java: 5. More on Classes / Java API

---
+++ Java/5. More on Classes/13.java
@@ -0,0 +1,12 @@
+import java.awt.*; // The wildcard character (*) is used to import all of the classes in the package
+
+class JavaAPI {
+    /*
+     * The Java API is a collection of classes and interfaces that have been written
+     * for developers to use.
+     * 
+     * All Java docs:
+     * http://docs.oracle.com/javase/7/docs/api/
+     */
+    public static void main(String[] args) {}
+}
